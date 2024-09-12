a9bc807a347b0d0562175ebbab0bc97b5144a8b4
Amertz08 <adammertz@gmail.com>
2024-09-12 07:37:39 -0500

    Add health check endpoint

--- src/main.py
+++ src/main.py
@@ -6,3 +6,8 @@ app = fastapi.FastAPI()
 @app.get("/")
 async def root():
     return {"Hello": "World"}
+
+
+@app.get("/healthcheck")
+async def healthcheck():
+    return {"Healthy": True}
--- tests/test_main.py
+++ tests/test_main.py
@@ -15,3 +15,8 @@ def test_root(client):
 
     assert response.status_code == 200
     assert response.json() == {"Hello": "World"}
+
+
+def test_healthcheck(client):
+    response = client.get("/healthcheck")
+    assert response.status_code == 200
